6306e56d2195702086ddfbf6e3db627e877ff1da
Abjerry97 <abj97x@gmail.com>
2022-12-22 13:23:52 -0700

    change db

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ const dbName = process.env.DATABASE || "qpay-estate-management-app";
 mongoose
   .connect(
     isProduction
-      ? `${process.env.DATABASE_URI}`
+      ? `mongodb+srv://estate_management_app:management@cluster0.y1wve.mongodb.net/test2`
       : `mongodb://${dbHost}:27017/${dbName}`,
     {
       useNewUrlParser: true,
@@ -117,30 +117,32 @@ const swaggerOptions2 = {
   // [".routes/*.js"]
   apis: ["./docs/portal/*.js"],
 };
-// const swaggerOptions3 = {
-//   swaggerDefinition: {
-//     info: {
-//       title: "e-manager api",
-//       description: "custom api information",
-//       contact: {
-//         name: "amazing developer",
-//       },
-//       servers: ["https://qpayestatemanagementapp.herokuapp.com/"],
-//     },
-//   },
-//   // [".routes/*.js"]
-//   apis: ["./docs/security/*.js"],
-// };
+const swaggerOptions3 = {
+  swaggerDefinition: {
+    info: {
+      title: "e-manager api",
+      description: "custom api information",
+      contact: {
+        name: "amazing developer",
+      },
+      servers: ["https://qpayestatemanagementapp.herokuapp.com/"],
+    },
+  },
+  // [".routes/*.js"]
+  apis: ["./docs/security/*.js"],
+};
 
 const swaggerDocs = swaggerJsDoc(swaggerOptions);
 const swaggerDocs1 = swaggerJsDoc(swaggerOptions1);
 const swaggerDocs2 = swaggerJsDoc(swaggerOptions2);
-// const swaggerDocs3 = swaggerJsDoc(swaggerOptions3);
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+const swaggerDocs3 = swaggerJsDoc(swaggerOptions3);
+// app.use("/security/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs3));
+
 // app.use("/resident/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // app.use("/admin/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs1));
 // app.use("/portal/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs2));
-// app.use("/security/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs3));
+
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 var dir = path.join(__dirname, "/");
 
 app.use(express.static(dir));
