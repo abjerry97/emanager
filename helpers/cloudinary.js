4416d4d7d1968df89ad463092d30ce125b0e7827
Abiodun Jeremiah <52524245+abjerry97@users.noreply.github.com>
2025-02-11 10:39:56 +0100

    Update cloudinary.js

--- helpers/cloudinary.js
+++ helpers/cloudinary.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({ 
-    cloud_name: 'qlab', 
-    api_key: '749422311556775', 
-    api_secret: 'KhjEuggX6OwEGfvtO24qQfnsBdA' 
+    cloud_name: 'process.env.CLOUDINARY_NAME', 
+    api_key: 'process.env.CLOUDINARY_API_KEY', 
+    api_secret: 'process.env.CLOUDINARY_API_SECRET' 
   });
   
   module.exports = cloudinary
